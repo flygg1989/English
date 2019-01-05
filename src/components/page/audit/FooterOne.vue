cd5964625e747aeb5ee1acb2344ae8f317243b5b
huangenhao <1258455670@qq.com>
2019-01-05 19:09:23 +0800

    1.5.2

---
+++ src/components/page/audit/FooterOne.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="table-footer">
+    <div class="footer-left">
+      <el-button plain type="primary">刷新</el-button>
+      <el-button plain type="primary">转办</el-button>
+      <el-button plain type="primary">不予处理</el-button>
+    </div>
+    <div class="footer-center">
+      <el-button>隐藏</el-button>
+    </div>
+    <div class="footer-right">
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        layout="sizes, prev, pager, next"
+        :total="100">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name:'FooterOne',
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  },
+  data() {
+    return {
+      currentPage:1
+    };
+  }
+}
+</script>
+<style scoped>
+  
+</style>
